7b13edf2375c662deb2f94982c98d6fd562ff6b5
PrunusSerrulata <say635231439@gmail.com>
2019-12-27 18:35:36 +0800

    Update newspider.py

--- newspider.py
+++ newspider.py
@@ -25,23 +25,24 @@ for poemnum in range(beg, end + 1):
     if len(spr.select('div[class="main3"]')[0].select('div[class="shileft"]')) <= 0:
         print("该诗不存在")
         continue
-    title = re.sub(r"[\f\n\r\s]", "", spr.select('div[class="main3"]')[0].select('div[class="shileft"]')[0].select('div[class="son1"]')[0].text)
-    print("标题: " + title, end=" ")
-    _cont, cont = spr.select('div[class="main3"]')[0].select('div[class="shileft"]')[0].select('div[class="son2"]')[0].find_all('p')[3:-1], ""
-    for i in _cont:
-        cont += i.text
-    cont = re.sub(r"[\f\n\r\s]", "", cont)
-    sents = re.split(r"[，。,.]", cont)[:-1]
-    for i in sents:
-        if len(i) != 5:
-            valid = False
-            break
-        valid = True
-    if valid:
-        f.write(title + ":" + cont + "\n")
-        print("完成")
     else:
-        print("非五言诗，丢弃")
+        title = re.sub(r"[\f\n\r\s]", "", spr.select('div[class="main3"]')[0].select('div[class="shileft"]')[0].select('div[class="son1"]')[0].text)
+        print("标题: " + title, end=" ")
+        _cont, cont = spr.select('div[class="main3"]')[0].select('div[class="shileft"]')[0].select('div[class="son2"]')[0].find_all('p')[3:-1], ""
+        for i in _cont:
+            cont += i.text
+        cont = re.sub(r"[\f\n\r\s]", "", cont)
+        sents = re.split(r"[，。,.]", cont)[:-1]
+        for i in sents:
+            if len(i) != 5:
+                valid = False
+                break
+            valid = True
+        if valid:
+            f.write(title + ":" + cont + "\n")
+            print("完成")
+        else:
+            print("非五言诗，丢弃")
     
     if poemnum != end:
         wait_sec = round(random.uniform(1, 2), 2)
